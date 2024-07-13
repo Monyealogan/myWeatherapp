dce116471d80c895ee14ec8e3d779a7afec6d0aa
Monyealogan <monyealogan12@gmail.com>
2024-07-12 21:32:36 -0400

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity(city) {
+    let apiKey = "fftb054daebc038f2o6666f4739b3ef3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+  
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Detroit");
